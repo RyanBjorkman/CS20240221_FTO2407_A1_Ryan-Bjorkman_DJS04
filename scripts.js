1a560ea71f8413ed914a10c5fdba9ce4e1877cf8
Ryan Bjorkman <ryanbjorkman@icloud.com>
2024-11-03 20:56:17 +0200

    modified renderBooks function to use the new book review web component

--- scripts.js
+++ scripts.js
@@ -37,19 +37,15 @@ function renderBooks(booksToRender) {
     const fragment = document.createDocumentFragment();
 
     for (const { author, id, image, title } of booksToRender) {
-        const element = document.createElement('button');
-        element.classList = 'preview';
-        element.setAttribute('data-preview', id);
+        const bookPreview = document.createElement('book-preview');
+        
+        // Set attributes for the BookPreview component
+        bookPreview.setAttribute('title', title);
+        bookPreview.setAttribute('author', authors[author]);
+        bookPreview.setAttribute('image', image);
+        bookPreview.setAttribute('id', id);
 
-        element.innerHTML = `
-            <img class="preview__image" src="${image}" />
-            <div class="preview__info">
-                <h3 class="preview__title">${title}</h3>
-                <div class="preview__author">${authors[author]}</div>
-            </div>
-        `;
-
-        fragment.appendChild(element);
+        fragment.appendChild(bookPreview);
     }
 
     document.querySelector('[data-list-items]').appendChild(fragment);
